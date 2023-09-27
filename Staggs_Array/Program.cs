7e169906125e481b30973018ad2285fe799476d4
GiftedR <kidsgamers@hotmail.com>
2023-09-27 12:55:08 -0700

    removed write function, removed storedNum variable, uses new character instead of bracket

--- Staggs_Array/Program.cs
+++ Staggs_Array/Program.cs
@@ -11,54 +11,38 @@ namespace Staggs_Array
         static void Main(string[] args)
         {
             char[] randChars = new char[50];//output array
-            int storedNumbers = 0;
             Random randomNum = new Random(DateTime.Now.Millisecond);
             Stopwatch sw = new Stopwatch();
             sw.Start();
-            while (storedNumbers < 50)
+            while (getIndex(randChars, '\0') != -1)
             {
-                char tempLetter = addLetter();
-                if (tempLetter != '[')
-                {
-                    randChars[storedNumbers] = tempLetter;
-                    storedNumbers++;
-                }
+                randChars[getIndex(randChars, '\0')] = addLetter();
             }
             char addLetter()
             {
                 char testChar = (char)(randomNum.Next(0, 26) + 65 + (32 * randomNum.Next(0, 2)));
-                if (valExists(randChars, testChar) == false)
+                if (getIndex(randChars, testChar) == -1)
                 {
                     return testChar;
                 }
                 else
                 {
-                    return '[';
+                    return '\0';
                 }
             }
-            bool valExists(char[] array, char value)
+            int getIndex(char[] array, char value)
             {
                 for (int i = 0; i < array.Length; i++)
                 {
                     if (array[i] == value)
                     {
-                        return true;
+                        return i;
                     }
                 }
-                return false;
+                return -1;
             }
             sw.Stop();
             Console.WriteLine(sw.Elapsed.ToString());
-            //Only to write the output
-            //writeArray();
-            void writeArray()
-            {
-                for (int i = 0; i < randChars.Length; i++)
-                {
-                    Console.Write(randChars[i]);
-                }
-                Console.Write('\n');
-            }
         }
     }
 }
